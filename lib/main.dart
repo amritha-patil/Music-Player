1b1576df569813e37dbb1e8df6d57a30f2d86b7f
Amritha Patil <amritha.bpatil@gmail.com>
2021-03-28 18:51:24 +0530

    Music Player

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MusicApp(),
+    );
+  }
+}
+
+class MusicApp extends StatefulWidget {
+  @override
+  _MusicAppState createState() => _MusicAppState();
+}
+
+class _MusicAppState extends State<MusicApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomLeft,
+                    colors: [Colors.black87, Colors.red[900]]))));
+  }
+}
